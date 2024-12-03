b5abf2fdbc34187e45bdbeb8184b83345f1ab4e3
Emma Rafkin <Emma.Rafkin@jhuapl.edu>
2024-12-03 17:29:54 -0500

    pushing to run on hpc

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-from scripts.utils.surprisal import calculate_abbreviation_surprisal, calculate_phrase_surprisal
+from scripts.utils.surprisal import calculate_phrase_surprisal
 from scripts.utils.abbreviations import abbrevations
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from datasets import load_dataset
 from tqdm import tqdm
+from scripts.utils.TweetNormalizer import normalizeTweet
+
 
 
 def get_calcuations(abbreviation_dict, tokenizer, model, dataset):
@@ -17,13 +19,22 @@ def get_calcuations(abbreviation_dict, tokenizer, model, dataset):
         count_abbr = 0
         tweets_with_abbr = []
         tweets_with_fl = []
-        fl_tokenized = tokenizer.tokenize(full_length)
-        abbr_tokenized =  tokenizer.tokenize(abbr)
+        fl_normalized = normalizeTweet(full_length)
+        fl_tokenized = tokenizer.tokenize(fl_normalized)
+        abbr_normalized = normalizeTweet(abbr)
+        abbr_tokenized =  tokenizer.tokenize(abbr_normalized)
         for split in dataset:
             for i in range(len(dataset[split])):
-                tokens = tokenizer.tokenize(dataset[split][i]["text"])
+                normalized_line = normalizeTweet(tweet=dataset[split][i]["text"])
+                normalized_line = normalized_line.lower()
+                normalized_line = normalized_line.replace("'", "")
+                tokens = tokenizer.tokenize(normalized_line)
                 tokens = [t.lower() for t in tokens]
                 tokens = [t.replace("'", "") for t in tokens]
+                fl_tokenized = [t.lower() for t in fl_tokenized]
+                fl_tokenized = [t.replace("'", "") for t in fl_tokenized]
+                abbr_tokenized = [t.lower() for t in abbr_tokenized]
+                abbr_tokenized = [t.replace("'", "") for t in abbr_tokenized]
                 fl_in_tweet = [tokens[idx: idx + len(fl_tokenized)] == fl_tokenized for idx in range(len(tokens) - len(fl_tokenized) + 1)].count(True)
                 abbr_in_tweet = [tokens[idx: idx + len(abbr_tokenized)] == abbr_tokenized for idx in range(len(tokens) - len(abbr_tokenized) + 1)].count(True)
 
@@ -33,24 +44,73 @@ def get_calcuations(abbreviation_dict, tokenizer, model, dataset):
                 if abbr_in_tweet > 0:
                     count_abbr+= abbr_in_tweet
                     tweets_with_abbr.append(dataset[split][i]["text"])
+        abbr_surprisals_first_token= []
+        abbr_surprisals_mask_all= []
+        abbr_surprisals_sequential= []
+        for tweet in tqdm(tweets_with_abbr):
+            ft, ma, seq = calculate_phrase_surprisal(model, tokenizer=tokenizer, tweet=tweet, phrase=abbr)
+            abbr_surprisals_first_token += ft
+            abbr_surprisals_mask_all += ma
+            abbr_surprisals_sequential += seq
+        abbr_surprisals_first_token = np.array(abbr_surprisals_first_token)
+        abbr_surprisals_mask_all = np.array(abbr_surprisals_mask_all)
+        abbr_surprisals_sequential = np.array(abbr_surprisals_sequential)
 
-        abbr_surprisals = np.array([calculate_abbreviation_surprisal(model, tokenizer=tokenizer, tweet=tweet, word=abbr) for tweet in tweets_with_abbr])
-        abbr_surprisals = abbr_surprisals[~pd.isnull(abbr_surprisals)]
-        full_length_surprisals = np.array([calculate_phrase_surprisal(model, tokenizer=tokenizer, tweet=tweet, phrase=full_length) for tweet in tweets_with_fl])
-        full_length_surprisals = full_length_surprisals[~pd.isnull(full_length_surprisals)]
-        df_arr.append([abbr, full_length, count_abbr, count_fl, np.mean(abbr_surprisals), np.mean(full_length_surprisals)])
+        fl_surprisals_first_token= []
+        fl_surprisals_mask_all= []
+        fl_surprisals_sequential= []
+        for tweet in tqdm(tweets_with_fl):
+            ft, ma, seq = calculate_phrase_surprisal(model, tokenizer=tokenizer, tweet=tweet, phrase=full_length)
+            fl_surprisals_first_token += ft
+            fl_surprisals_mask_all += ma
+            fl_surprisals_sequential += seq
+        fl_surprisals_first_token = np.array(fl_surprisals_first_token)
+        fl_surprisals_mask_all = np.array(fl_surprisals_mask_all)
+        fl_surprisals_sequential = np.array(fl_surprisals_sequential)
+        print(fl_surprisals_first_token, fl_surprisals_mask_all, fl_surprisals_sequential)
+        df_arr.append([abbr, 
+                       full_length, 
+                       count_abbr, 
+                       count_fl, 
+                       np.mean(abbr_surprisals_first_token), 
+                       np.median(abbr_surprisals_first_token),
+                       np.mean(abbr_surprisals_mask_all), 
+                       np.median(abbr_surprisals_mask_all),
+                       np.mean(abbr_surprisals_sequential), 
+                       np.median(abbr_surprisals_sequential),
+                       np.mean(fl_surprisals_first_token), 
+                       np.median(fl_surprisals_first_token),
+                       np.mean(fl_surprisals_mask_all), 
+                       np.median(fl_surprisals_mask_all),
+                       np.mean(fl_surprisals_sequential), 
+                       np.median(fl_surprisals_sequential),
+                     ])
     
-    df = pd.DataFrame(df_arr, columns=["abbreviation", "full_length", "count_abbr", "count_fl", "surprisal_abbr", "surprisal_fl"])
+    df = pd.DataFrame(df_arr, columns=["abbreviation", 
+                                       "full_length", 
+                                       "count_abbr", 
+                                       "count_fl", 
+                                       "abbr_first_token_surprisal_mean", 
+                                       "abbr_first_token_surprisal_median", 
+                                       "abbr_mask_all_surprisal_mean", 
+                                       "abbr_mask_all_surprisal_median",
+                                       "abbr_sequential_surprisal_mean", 
+                                       "abbr_sequential_surprisal_median",
+                                       "fl_first_token_surprisal_mean", 
+                                       "fl_first_token_surprisal_median", 
+                                       "fl_mask_all_surprisal_mean", 
+                                       "fl_mask_all_surprisal_median",
+                                       "fl_sequential_surprisal_mean", 
+                                       "fl_sequential_surprisal_median"])
     return df
 
 if __name__ == "__main__":
     subset={
-        "brother":"bro", 
+        "i don't know": "idk", 
+        # "brother":"bro", 
         "oh my god": "omg",
-        "sorry": "sry", 
-        "about": "abt",
-        "direct message": "dm",
-        "retweet": "rt"
+        # "direct message": "dm",
+        # "retweet": "rt"
         }
     bertweet = AutoModelForMaskedLM.from_pretrained("vinai/bertweet-base")
     tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base")
--- scripts/utils/surprisal.py
+++ scripts/utils/surprisal.py
@@ -3,37 +3,13 @@ import numpy as np
 from scripts.utils.TweetNormalizer import normalizeTweet
 
 MAX_LENGTH = 128
-def calculate_abbreviation_surprisal(model, tokenizer, tweet, word):
-    # calculate surprisal for a given word based on a tweet
-    t = tweet.lower()
-    normalized_line = normalizeTweet(tweet=t)
-    words = tokenizer.tokenize(normalized_line)
-
-    mask_ids = [i for i in range(len(words)) if words[i] == word]    
-    surprisals = []
-    for mask_id in mask_ids:
-        words[mask_id] = tokenizer.mask_token
-        input_ids = tokenizer.encode(tokenizer.convert_tokens_to_string(words), padding="max_length", max_length=MAX_LENGTH)
-        if len(input_ids) > MAX_LENGTH:
-            min_idx = mask_ids[0]-int(MAX_LENGTH/2)
-            if min_idx<0:
-                min_idx=0
-            max_idx = mask_ids[0]+int(MAX_LENGTH/2)
-            if max_idx>len(input_ids):
-                max_idx = len(input_ids)-1
-            input_ids = input_ids[min_idx:max_idx]
-        logits = model(torch.tensor([input_ids])).logits
-        probs =  torch.nn.functional.softmax(logits[0, mask_id, :], dim=0)
-        word_idx = tokenizer.convert_tokens_to_ids(word)
-        surprisal = -torch.log2(probs[word_idx]).detach().numpy() # why is this sometimes nan?
-        surprisals.append(surprisal)
-    return np.mean(surprisals)
     
 def calculate_phrase_surprisal(model, tokenizer, tweet, phrase):
     t = tweet.lower()
     normalized_line = normalizeTweet(tweet=t)
     words = tokenizer.tokenize(normalized_line)
-    tokenized_phrase = tokenizer.tokenize(phrase)
+    normalized_phrase = normalizeTweet(tweet=phrase)
+    tokenized_phrase = tokenizer.tokenize(normalized_phrase)
     mask_ids = []
     temp_phrase_mask_ids = [i for i in range(len(words)) if words[i] == tokenized_phrase[0]]
     phrase_mask_ids = []
@@ -41,35 +17,81 @@ def calculate_phrase_surprisal(model, tokenizer, tweet, phrase):
         full_phrase_in_tweet = True
         phrase_masks = []
         for idx, token in enumerate(tokenized_phrase):
+            t = token
+            if "'" in t:
+                t = t.replace("'", "")
+            
             phrase_masks.append(pmi + idx)
-            try:
-                if words[pmi + idx] != token:
+            if pmi + idx < len(words):
+                try:
+                    pt = words[pmi + idx]
+                    if "'" in pt:
+                        pt = pt.replace("'", "")
+                    if pt != t:
+                        full_phrase_in_tweet = False
+                except Exception as e:
+                    print("there was an exception")
+                    print(e)
+                    print(pmi, idx, words, phrase)
                     full_phrase_in_tweet = False
-            except Exception as e:
-                print("there was an exception")
-                print(e)
-                print(pmi, words, phrase)
-                full_phrase_in_tweet = False
         if full_phrase_in_tweet:
             phrase_mask_ids.append(phrase_masks)
-    surprisals = []
+    first_token_surprisals = []
+    mask_all_surprisals = []
+    sequential_mask_surprisals = []
+
     for phrase_mask_group in phrase_mask_ids:
-        phrase_surprisals = []
+        # first mask all tokens in the phrase (also works for a mono token situation like the abbreviations themselves)
+        words1 = words
         for pmi in phrase_mask_group:
-            words[pmi] = tokenizer.mask_token
-            input_ids = tokenizer.encode(tokenizer.convert_tokens_to_string(words), padding="max_length", max_length=MAX_LENGTH)
-            if len(input_ids) > MAX_LENGTH:
-                min_idx = mask_ids[0][0] - int(MAX_LENGTH/2)
-                if min_idx<0:
-                    min_idx=0
-                max_idx = mask_ids[0][0]+int(MAX_LENGTH/2)
-                if max_idx>len(input_ids):
-                    max_idx = len(input_ids)-1
-                input_ids = input_ids[min_idx:max_idx]
-            logits = model(torch.tensor([input_ids])).logits
+            words1[pmi] = tokenizer.mask_token
+
+        #get input ids and truncate if necessary
+        input_ids = tokenizer.encode(tokenizer.convert_tokens_to_string(words1), padding="max_length", max_length=MAX_LENGTH)
+        if len(input_ids) > MAX_LENGTH:
+            min_idx = mask_ids[0][0] - int(MAX_LENGTH/2)
+            if min_idx<0:
+                min_idx=0
+            max_idx = mask_ids[0][0]+int(MAX_LENGTH/2)
+            if max_idx>len(input_ids):
+                max_idx = len(input_ids)-1
+            input_ids = input_ids[min_idx:max_idx]
+        # get logits from model
+        logits = model(torch.tensor([input_ids])).logits
+        phrase_surprisals = []
+        for idx, pmi in enumerate(phrase_mask_group):
+            # get prob for the specific phrase token
             probs =  torch.nn.functional.softmax(logits[0, pmi, :], dim=0)
-            word_idx = tokenizer.convert_tokens_to_ids(tokenized_phrase[0])
+            word_idx = tokenizer.convert_tokens_to_ids(tokenized_phrase[idx])
             surprisal = -torch.log2(probs[word_idx]).detach().numpy()
+            if idx == 0:
+                first_token_surprisals.append(surprisal)
             phrase_surprisals.append(surprisal)
-        surprisals.append(np.mean(phrase_surprisals))
-    return np.mean(surprisals)
+        mask_all_surprisals.append(np.sum(phrase_surprisals))
+
+        #now do the sequential mapping if possible
+        words1 = words
+        if len(tokenized_phrase) == 1:
+            # it is all the same
+            sequential_mask_surprisals = mask_all_surprisals
+        else:
+            for pmi in phrase_mask_group:
+                # mask just the one token, leave the rest
+                words1[pmi] = tokenizer.mask_token
+                input_ids = tokenizer.encode(tokenizer.convert_tokens_to_string(words1), padding="max_length", max_length=MAX_LENGTH)
+                if len(input_ids) > MAX_LENGTH:
+                    min_idx = mask_ids[0][0] - int(MAX_LENGTH/2)
+                    if min_idx<0:
+                        min_idx=0
+                    max_idx = mask_ids[0][0]+int(MAX_LENGTH/2)
+                    if max_idx>len(input_ids):
+                        max_idx = len(input_ids)-1
+                    input_ids = input_ids[min_idx:max_idx]
+                logits = model(torch.tensor([input_ids])).logits
+                probs =  torch.nn.functional.softmax(logits[0, pmi, :], dim=0)
+                word_idx = tokenizer.convert_tokens_to_ids(tokenized_phrase[0])
+                surprisal = -torch.log2(probs[word_idx]).detach().numpy()
+                phrase_surprisals.append(surprisal)
+            sequential_mask_surprisals.append(np.sum(phrase_surprisals))
+        
+    return first_token_surprisals, mask_all_surprisals, sequential_mask_surprisals
